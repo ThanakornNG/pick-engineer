3c922c2354fc05a91516a72d276830092564e569
Thanakorn <podmeax@gmail.com>
2014-10-29 23:33:04 -0300

    add comment

--- picker.rb
+++ picker.rb
@@ -1,6 +1,6 @@
 def pick_engineer()
-	engineer=File.read("engineers.txt").split("\n")
-	name=engineer[rand(engineer.size)]
+	engineer=File.read("engineers.txt").split("\n")#read text file
+	name=engineer[rand(engineer.size)]#random some name
 	return name
 end
 puts pick_engineer()
